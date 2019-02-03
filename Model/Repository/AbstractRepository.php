81682ea2ab48e792379e3fd0ab69d4946df5bccd
vova <wayses@gmail.com>
2019-02-04 01:00:02 +0200

    repository method for transform into object added

--- Model/Repository/AbstractRepository.php
+++ Model/Repository/AbstractRepository.php
@@ -10,6 +10,11 @@ use App\MysqlPDOConnection;
 abstract class AbstractRepository
 {
     /**
+     * @var
+     */
+    protected $entityClass;
+
+    /**
      * @var MysqlPDOConnection
      */
     protected $connection;
@@ -21,5 +26,46 @@ abstract class AbstractRepository
     public function __construct(MysqlPDOConnection $connection)
     {
         $this->connection = $connection->getConnection();
+    }
+
+    /**
+     * @param array $record
+     * @return AbstractRepository
+     */
+    protected function prepareEntity(array $record)
+    {
+        $class = $this->getEntityClass();
+        $obj = new $class;
+        $objVars = get_object_vars($obj);
+/*var_dump($objVars);
+var_dump($record);
+        exit;*/
+        foreach ($objVars as $k => $value) {
+            $obj->{$k} = $record[$k];
+        }
+        return $obj;
+    }
+
+    /**
+     * @param array $records
+     * @return array
+     */
+    protected function prepareEntities(array $records)
+    {
+        $result = [];
+
+        foreach ($records as $record) {
+            $result[] = $this->prepareEntity($record);
+        }
+
+        return $result;
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function getEntityClass()
+    {
+        return $this->entityClass;
     }
 }
